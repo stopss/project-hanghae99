bfed04c7a48831fd96f4903a39ced2ab0424a6ea
nwnp <jjw_0418@naver.com>
2022-06-27 22:13:53 +0900

    CI TEST

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8080);
-  console.log('8080번에서 대기중....');
+  console.log('8080번에서 대기중.....');
 }
 bootstrap();
