d4f7c7312de0586adb17a9b5dd410ef460c30934
nwnp <jjw_0418@naver.com>
2022-06-27 20:23:40 +0900

    feat: create dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:10 As builder
+WORKDIR /app
+COPY package.json ./
+RUN npm install
+COPY ./ ./
+RUN npm run build
+
+FROM node:10-alpine
+WORKDIR /app
+COPY --from=builder /app ./
+CMD ["npm", "run", "start"]
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8080);
+  console.log('8080번에서 대기중...');
 }
 bootstrap();
