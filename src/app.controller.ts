d5ea28e2a4b33bdb9909250f5eef59dbc8975f42
nwnp <jjw_0418@naver.com>
2022-06-27 23:32:37 +0900

    bug

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
+// import { AppService } from './app.service';
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller()
+@Controller('/')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    // return this.appService.getHello();
+    return 'hello world!';
   }
 }
