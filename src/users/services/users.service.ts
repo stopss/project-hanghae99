29c04d7139cb018b945cb3916877d823640f02df
nwnp <jjw_0418@naver.com>
2022-06-29 19:46:02 +0900

    fix: JWT Token 발급

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,3 +1,4 @@
+import { JwtService } from '@nestjs/jwt';
 import { Inject, Injectable, HttpException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { SignupUserDto } from '../dto/signup.request.dto';
@@ -9,6 +10,7 @@ export class UsersService {
   constructor(
     @Inject('USER_REPOSITORY')
     private readonly usersRepository: Repository<UserEntity>,
+    private readonly jwtService: JwtService,
   ) {}
 
   async findUserByEmail(email: string) {
@@ -69,9 +71,18 @@ export class UsersService {
       const { email, nickname, refreshToken } = body;
       const userExist = await this.usersRepository.find({ where: { email } });
       if (userExist.length !== 0) {
-        const temp = { ...userExist[0] };
         await this.usersRepository.update({ email }, { refreshToken });
-        return { result: { success: true, ...temp } };
+        const user = await this.findUserByEmail(email);
+        console.log(user);
+        const payload = {
+          id: user.id,
+          email: user.email,
+          nickname: user.nickname,
+          refreshToken: user.refreshToken,
+          social: user.refreshToken,
+        };
+        const token = this.jwtService.sign(payload);
+        return { result: { success: true, token } };
       }
       user.email = email;
       user.nickname = nickname;
@@ -79,10 +90,18 @@ export class UsersService {
       user.social = true;
       user.password = null;
       const newUser = await this.usersRepository.save(user);
-      const result = { result: { success: true, ...newUser } };
-      return result;
+      const payload = {
+        id: newUser.id,
+        email: newUser.email,
+        nickname: newUser.nickname,
+        refreshToken: newUser.refreshToken,
+        social: newUser.refreshToken,
+      };
+      const token = this.jwtService.sign(payload);
+      return {
+        result: { success: true, token },
+      };
     } catch (error) {
-      console.error(error);
       throw new HttpException('서버 에러', 500);
     }
   }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,18 @@ import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 import { userProviders } from './models/users.provider';
 import { AuthModule } from 'src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/auth/jwt/constants';
 
 @Module({
-  imports: [DatabaseModule, forwardRef(() => AuthModule)],
+  imports: [
+    DatabaseModule,
+    forwardRef(() => AuthModule),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1y' },
+    }),
+  ],
   providers: [UsersService, ...userProviders],
   controllers: [UsersController],
   exports: [UsersService],
