def54d7e68ed3524cd6932ee7cc6f90f131745b7
nwnp <jjw_0418@naver.com>
2022-07-25 23:05:46 +0900

    feat: Web Security setup - helmet, csurf, https

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,18 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
+import helmet from 'helmet';
+import * as fs from 'fs';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/private-key.pem'),
+    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  };
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
+    httpsOptions,
   });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
@@ -15,9 +23,12 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '../views'));
   app.useStaticAssets(join(__dirname, '../public'));
   app.setViewEngine('hbs');
+  app.use(helmet());
+  app.use(csurf());
+
   const PORT = process.env.PORT;
   await app.listen(PORT);
   const logger = new Logger('MAIN');
-  logger.log(`The server is on PORT ${PORT}`);
+  logger.log(`The server is on`);
 }
 bootstrap();
