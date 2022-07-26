f91367d196d4b1ab10063d7153dc2fdaf84c5c87
nwnp <jjw_0418@naver.com>
2022-07-27 01:39:19 +0900

    fix: mysql port

--- src/common/database/database.provider.ts
+++ src/common/database/database.provider.ts
@@ -12,7 +12,7 @@ export const databaseProviders = [
       const dataSource = new DataSource({
         type: 'mysql',
         host: process.env.DATABASE_HOST,
-        port: 3305,
+        port: 3306,
         username: process.env.DATABASE_USERNAME,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE,
