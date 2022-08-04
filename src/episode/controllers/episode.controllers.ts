16a58b071c47d6e6672d379136897b953138e326
nwnp <jjw_0418@naver.com>
2022-08-04 17:15:50 +0900

    feat: auth

--- src/episode/controllers/episode.controllers.ts
+++ src/episode/controllers/episode.controllers.ts
@@ -1,14 +1,16 @@
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 import { EpisodeService } from 'src/episode/services/episode.service';
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Controller, Get, Logger, Req, UseGuards } from '@nestjs/common';
 
 @Controller('roles')
 export class EpisodeController {
   constructor(private readonly episodeService: EpisodeService) {}
 
   @Get()
-  getRole() {
+  @UseGuards(JwtAuthGuard)
+  getRole(@Req() req) {
     const logger = new Logger('DEBUG');
-    logger.debug('GET ROLES');
+    logger.debug(`[GET ROLES] ${req.user}`);
     return this.episodeService.allRole();
   }
 }
