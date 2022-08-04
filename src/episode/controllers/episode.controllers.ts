0390afac5b40b94d17607981ced90376037c42a6
nwnp <jjw_0418@naver.com>
2022-08-04 17:22:56 +0900

    fix

--- src/episode/controllers/episode.controllers.ts
+++ src/episode/controllers/episode.controllers.ts
@@ -1,12 +1,16 @@
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 import { EpisodeService } from 'src/episode/services/episode.service';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Logger, Req } from '@nestjs/common';
 
 @Controller('roles')
 export class EpisodeController {
   constructor(private readonly episodeService: EpisodeService) {}
 
   @Get()
-  getRole() {
+  @UseGuards(JwtAuthGuard)
+  getRole(@Req() req) {
+    const logger = new Logger('DEBUG');
+    logger.debug(`[GET ROLES] ${req.user.email}`);
     return this.episodeService.allRole();
   }
 }
--- src/episode/episode.module.ts
+++ src/episode/episode.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { DatabaseModule } from 'src/common/database/database.module';
 import { EpisodeController } from './controllers/episode.controllers';
 import { episodeProviders } from './episode.provider';
 import { EpisodeService } from './services/episode.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   providers: [EpisodeService, ...episodeProviders],
   controllers: [EpisodeController],
   exports: [EpisodeService],
